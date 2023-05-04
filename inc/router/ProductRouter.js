fe6bd16f024efd31c8941355f51ff70b37649e43
Sheff2000 <sheffpb@gmail.com>
2023-05-04 16:17:35 +0300

    маршруты обновил

--- inc/router/ProductRouter.js
+++ inc/router/ProductRouter.js
@@ -5,28 +5,30 @@ import OrderController from '../controller/OrderController.js';
 
 const router = new Router;
 
-
 const rout = [
-    { method: 'get', path: '/', handler: ProductController.getMainPage }, // главная
-    { method: 'get', path: '/sweet/:idGroup', handler: ProductController.getProdctGroupInfo }, // страница вариациипродукта
-    { method: 'post', path: '/register', handler: ClientController.postSignIn }, // регистрация клиента
-    { method: 'get', path: '/login', handler: ClientController.getLogin },   // загрузка данных о клиенте по его номеру
-    { method: 'post', path: '/login', handler: ClientController.postLogIn },  // авторизация клиента
-    { method: 'put', path: '/login', handler: ClientController.putLogin },  // обновление данных о клиенте (подверждение почты например)
-    { method: 'get', path: '/order/status/:idOrder', handler: OrderController.getOrderStatus },  //инвормация о статусе заказа и продуктов в нем
-    { method: 'put', path: '/order/buy/:idOrder', handler: OrderController.putBuyOrderStatus },  // сохранение данных о результатах оплаты заказа
-    { method: 'get', path: '/order/buy/:idOrder', handler: OrderController.getBuyOrderStatus },  // получение данных о результатах оплаты заказа
-    { method: 'post', path: '/order/processing', handler: OrderController.postCartProcessingToOrder },  // получаем данные дл оформление заказа (датиа доставки, адресс ...) и записываем ее в БД
-    { method: 'get', path: '/cart', handler: OrderController.getCartProductInfo }  //отдаем инфу из БД о продукции в корзине/ Данные что в корзине получаем по гет-запросу в виде набора Айди и количества
+    { method: 'get',  path: '/',                      handler: ProductController.getMainPage }, // главная
+    { method: 'get',  path: '/sweet/:idGroup',        handler: ProductController.getProdctGroupInfo }, // страница вариациипродукта
+    { method: 'post', path: '/register',              handler: ClientController.postSignIn }, // регистрация клиента
+    { method: 'get',  path: '/login',                 handler: ClientController.getLogin },   // загрузка данных о клиенте по его номеру
+    { method: 'post', path: '/login/post',            handler: ClientController.postLogIn },  // авторизация клиента
+    { method: 'get',  path: '/order',                 handler: OrderController.getCartProductInfo },  //отдаем инфу из БД о продукции в корзине/ Данные что в корзине получаем по гет-запросу в виде набора Айди и количества
+    { method: 'get',  path: '/order/status/:idOrder', handler: OrderController.getOrderStatus },  //инвормация о статусе заказа и продуктов в нем
+    { method: 'post',  path: '/order/buy/:idOrder',   handler: OrderController.putBuyOrderStatus },  // сохранение данных о результатах оплаты заказа
+    { method: 'get',  path: '/order/buy/:idOrder',    handler: OrderController.getBuyOrderStatus },  // получение данных о результатах оплаты заказа
+    { method: 'post', path: '/order/processing',      handler: OrderController.postCartProcessingToOrder },  // получаем данные дл оформление заказа (датиа доставки, адресс ...) и записываем ее в БД
     
   ];
+  //{ method: 'put',  path: '/login/put',      handler: ClientController.putLogin },  // обновление данных о клиенте (подверждение почты например)
   
+
   rout.forEach((route) => {
     if (route.method === 'get') {
       router.get(route.path, route.handler);
-    } else if (route.method === 'post') {
+    } 
+    else if (route.method === 'post') {
       router.post(route.path, route.handler);
-    } else {
+    } 
+    else {
       // Если метод маршрута неизвестен, выдаем 404 ошибку
       router.use((req, res, next) => {
         res.status(404).send('Not Found');
